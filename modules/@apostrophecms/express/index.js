5fc3d8da662de833d08a2c8bb7d71afa7a202fd4
Matthew Francis Brunetti <zenflow87@gmail.com>
2021-10-20 05:28:58 -0400

    Set session secret

--- modules/@apostrophecms/express/index.js
+++ modules/@apostrophecms/express/index.js
@@ -1,8 +1,21 @@
 module.exports = {
   options: {
     session: {
-      // If this still says `undefined`, set a real secret!
       secret: undefined
     }
+  },
+  init (self) {
+    // set session secret at run-time, not build-time
+    self.options.session.secret = getSessionSecret();
   }
 };
+
+function getSessionSecret () {
+  if (process.env.SESSION_SECRET) {
+    return process.env.SESSION_SECRET;
+  }
+  if (process.env.NODE_ENV === 'production') {
+    throw new Error('Env var SESSION_SECRET must be set for production');
+  }
+  return 'session_secret';
+}
