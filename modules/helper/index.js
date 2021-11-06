fbadfb48fe4cb1c0833df89f7efea4da92a99eef
Matthew Francis Brunetti <zenflow87@gmail.com>
2021-11-06 01:31:23 -0400

    Add breadcrumbs view

--- modules/helper/index.js
+++ modules/helper/index.js
@@ -7,6 +7,19 @@ module.exports = {
   init(self) {
     self.addHelpers({
       ...linkHelpers,
+      jsonStringify(o) {
+        return JSON.stringify(o);
+      },
+      isDev() {
+        return process.env.NODE_ENV !== "production";
+      },
+      getPageAncestry(data) {
+        const items = [...data.page._ancestors, data.page];
+        if (data.piece) {
+          items.push(data.piece);
+        }
+        return items;
+      },
     });
   },
 };
