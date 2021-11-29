3dbbc9988555c164a566607da9d8330e63a916eb
Matthew Francis Brunetti <zenflow87@gmail.com>
2021-11-29 17:45:07 -0500

    Remove @apostrophecms/form module

--- app.js
+++ app.js
@@ -11,15 +11,6 @@ require("apostrophe")({
     // --- extensions ---
     "@apostrophecms/sitemap": {},
     "@apostrophecms/redirect": { options: { slugPrefix: "redirect-" } },
-    "@apostrophecms/form": { options: { slugPrefix: "form-" } },
-    "@apostrophecms/form-widget": {},
-    "@apostrophecms/form-text-field-widget": {},
-    "@apostrophecms/form-textarea-field-widget": {},
-    "@apostrophecms/form-select-field-widget": {},
-    "@apostrophecms/form-radio-field-widget": {},
-    "@apostrophecms/form-checkboxes-field-widget": {},
-    "@apostrophecms/form-boolean-field-widget": {},
-    "@apostrophecms/form-conditional-widget": {},
 
     // --- custom ---
     "init-db": {},
