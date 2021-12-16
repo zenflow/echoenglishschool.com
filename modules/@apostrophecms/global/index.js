f406ad9f40516e38a486d8c6e29b61b62fe3d771
Matthew Francis Brunetti <zenflow87@gmail.com>
2021-12-15 22:11:01 -0500

    Add global fields for footer

--- modules/@apostrophecms/global/index.js
+++ modules/@apostrophecms/global/index.js
@@ -1,3 +1,26 @@
+function getLinksField() {
+  return {
+    type: "array",
+    titleField: "text",
+    fields: {
+      add: {
+        text: {
+          type: "string",
+          required: true,
+        },
+        url: {
+          type: "string",
+          required: true,
+        },
+        openLinkInNewWindow: {
+          type: "boolean",
+          def: false,
+        },
+      },
+    },
+  };
+}
+
 module.exports = {
   fields: {
     add: {
@@ -6,11 +29,52 @@ module.exports = {
         type: "string",
         def: "ECHO English School",
       },
+      footerBlurbTitle: {
+        type: "string",
+        def: "ECHO English School",
+      },
+      footerBlurbContent: {
+        type: "area",
+        options: {
+          max: 1,
+          widgets: {
+            "@apostrophecms/rich-text": {},
+          },
+        },
+      },
+      footerLinkLists: {
+        type: "array",
+        titleField: "title",
+        fields: {
+          add: {
+            title: {
+              type: "string",
+              required: true,
+            },
+            links: getLinksField(),
+          },
+        },
+      },
+      footerCopyright: {
+        type: "string",
+        def: "© ECHO English School, All Right Reserved",
+      },
+      footerLegalLinks: getLinksField(),
     },
     group: {
       general: {
         label: "General",
         fields: ["siteTitle"],
+      },
+      footer: {
+        label: "Footer",
+        fields: [
+          "footerBlurbTitle",
+          "footerBlurbContent",
+          "footerLinkLists",
+          "footerCopyright",
+          "footerLegalLinks",
+        ],
       },
     },
   },
